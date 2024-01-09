5ebac2756bc31be06819e3a4ea081bee8769497d
Dylan <dylan.p@tiflabs.in>
2024-01-09 20:08:46 +0400

    added modal component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import ExpenseCategoryItem from '@/components/ExpenseCategoryItem';
+import ModalDialog from '@/components/ModalDialog';
 import { currencyFormatter } from '@/lib/utils';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { useState } from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -21,51 +23,73 @@ const SAMPLE_DATA = [
 ];
 
 export default function Home() {
+	const [isOpen, setIsOpen] = useState(false);
+	const closeModal = () => {
+		setIsOpen(false);
+	};
+
+	const openModal = () => {
+		setIsOpen(true);
+	};
 	return (
-		<main className="container max-w-2xl p-6 mx-auto">
-			<section className="py-3">
-				<small className="text-gray-400 text-md">My Balance</small>
-				<h2 className="text-4xl font-bold">{currencyFormatter(10000000)}</h2>
-			</section>
+		<>
+			<ModalDialog
+				heading="Add Expense"
+				isOpen={isOpen}
+				closeModal={closeModal}
+			>
+				<p className="text-sm text-gray-200">
+					Your payment has been successfully submitted. We’ve sent you an email
+					with all of the details of your order.
+				</p>
+			</ModalDialog>
+			<main className="container max-w-2xl p-6 mx-auto">
+				<section className="py-3">
+					<small className="text-gray-400 text-md">My Balance</small>
+					<h2 className="text-4xl font-bold">{currencyFormatter(10000000)}</h2>
+				</section>
 
-			<section className="flex items-center gap-2 py-3">
-				<button className="btn btn-primary">+ Expense</button>
-				<button className="btn btn-primary-outline">+ Income</button>
-			</section>
+				<section className="flex items-center gap-2 py-3">
+					<button className="btn btn-primary" onClick={() => openModal()}>
+						+ Expense
+					</button>
+					<button className="btn btn-primary-outline">+ Income</button>
+				</section>
 
-			<section className="py-3">
-				<h3 className="text-2xl">My Expenses</h3>
-				<div className="flex flex-col gap-4 mt-6">
-					{SAMPLE_DATA.map(({ title, color, total }, index) => (
-						<ExpenseCategoryItem
-							key={`expense-${index}`}
-							title={title}
-							total={total}
-							color={color}
+				<section className="py-3">
+					<h3 className="text-2xl">My Expenses</h3>
+					<div className="flex flex-col gap-4 mt-6">
+						{SAMPLE_DATA.map(({ title, color, total }, index) => (
+							<ExpenseCategoryItem
+								key={`expense-${index}`}
+								title={title}
+								total={total}
+								color={color}
+							/>
+						))}
+					</div>
+				</section>
+
+				<section className="py-6">
+					<h3 className="text-2xl">Statistics</h3>
+					<div className="w-1/2 mx-auto">
+						<Doughnut
+							data={{
+								labels: SAMPLE_DATA.map(({ title }) => title),
+								datasets: [
+									{
+										label: 'Expenses',
+										data: SAMPLE_DATA.map(({ total }) => total),
+										backgroundColor: SAMPLE_DATA.map(({ color }) => color),
+										borderColor: ['#18181b'],
+										borderWidth: 5,
+									},
+								],
+							}}
 						/>
-					))}
-				</div>
-			</section>
-
-			<section className="py-6">
-				<h3 className="text-2xl">Statistics</h3>
-				<div className="w-1/2 mx-auto">
-					<Doughnut
-						data={{
-							labels: SAMPLE_DATA.map(({ title }) => title),
-							datasets: [
-								{
-									label: 'Expenses',
-									data: SAMPLE_DATA.map(({ total }) => total),
-									backgroundColor: SAMPLE_DATA.map(({ color }) => color),
-									borderColor: ['#18181b'],
-									borderWidth: 5,
-								},
-							],
-						}}
-					/>
-				</div>
-			</section>
-		</main>
+					</div>
+				</section>
+			</main>
+		</>
 	);
 }
